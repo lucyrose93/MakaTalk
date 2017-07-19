4108b505d41caef79c0c6cb1f1cadfde33e8f586
lucyrose93 <lucyrose@protonmail.com>
2017-07-19 15:12:25 +0100

    set up basic server
    relates #37

---
+++ src/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+// Add the route
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('../public/index.html');
+        }
+    });
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
