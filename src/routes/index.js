455be8959137d4c8e324cac7eca2f77a77957975
lucyrose93 <lucyrose@protonmail.com>
2017-07-20 12:43:17 +0100

    create routes
    relates #39

---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+module.exports = [
+  './post-session.js',
+  './pre-session.js',
+  './static.js'
+].map(require);
--- src/server.js
+++ src/server.js
@@ -1,44 +1,18 @@
-'use strict';
+const hapi = require('hapi');
+const inert = require('inert');
+const vision = require('vision');
 
-const Hapi = require('hapi');
+const routes = require('./routes');
 
-// Create a server with a host and port
-const server = new Hapi.Server();
+const server = new hapi.Server();
+
 server.connection({
-    host: 'localhost',
-    port: 8000
+  port: process.env.PORT || 3000,
 });
 
-// Add the route
-server.register(require('inert'), (err) => {
-
-    if (err) {
-        throw err;
-    }
-
-    server.route({
-        method: 'GET',
-        path: '/hello',
-        handler: function (request, reply) {
-            reply.file('./public/index.html');
-        }
-    });
+server.register([inert, vision], (err) => {
+  if (err) throw err;
+  server.route(routes);
 });
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
-
-
-// Start the server
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
-});
+module.exports = server;
