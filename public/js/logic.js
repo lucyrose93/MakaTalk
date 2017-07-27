f9e7c497f70327d3f27c3110b448d9c4fdcc80ca
smarthutza <smarthutza@yahoo.com>
2017-07-27 14:50:36 +0100

    Started workig on the switch statement that changes the pages

---
+++ public/js/logic.js
@@ -0,0 +1,18 @@
+// function changePage () {
+//   var currentPage = location.pathname.slice(1)
+//   console.log(currentPage)
+//   switch (currentPage) {
+//     case 'understand': alert('Understand')
+//       break
+//     case 'help': console.log('Help')
+//       break
+//     case 'like': console.log('like')
+//       break
+//     case 'come-again': console.log('come again')
+//       break
+//   }
+// }
+//
+// document.querySelectorAll('figure.five-videos').forEach((e, i) => {
+//   e.addEventListener('click', changePage)
+// })
