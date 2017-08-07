c9407942f9ebbc7b72d0933968b04ce8c0695137
lucyrose93 <lucyrose@protonmail.com>
2017-08-07 19:46:32 +0100

    display results array on results pg
    relates #69

---
+++ public/get-results.js
@@ -0,0 +1,34 @@
+/* global: location, sessionStorage */
+
+(function resultsPage () {
+  var path = location.pathname.slice(1)
+    if (path === "results") {
+      getSurveyResults ()
+    }
+    else return
+  })();
+
+function getSurveyResults () {
+  var insertAnswers = document.getElementById('insert-answers')
+  var surveyType = sessionStorage.getItem('select-survey')
+
+    if (surveyType === 'pre-session') {
+      var preSessionResponses = ['consent', 'today', 'last-week', 'family', 'friends', 'school', 'play', 'next-week']
+      var preSessionAnswers = []
+        for (var i = 0; i < preSessionResponses.length; i++){
+          preSessionAnswers.push(sessionStorage.getItem(preSessionResponses[i]))
+        }
+        insertAnswers.innerHTML = preSessionAnswers
+    }
+    else if (surveyType === 'post-session') {
+      var postSessionResponses = ['consent', 'understand', 'help', 'like', 'come-again']
+      var postSessionAnswers = []
+        for (var i = 0; i < postSessionResponses.length; i++){
+          postSessionAnswers.push(sessionStorage.getItem(postSessionResponses[i]))
+        }
+        insertAnswers.innerHTML = postSessionAnswers
+    }
+    else {
+      insertAnswers.innerHTML = 'no results available'
+    }
+}
