1da5f1c124568517ffb8297c7a0c3c0e917a8d8e
lucyrose93 <lucyrose@protonmail.com>
2017-08-08 22:26:02 +0100

    add survey type to results pg

--- public/get-results.js
+++ public/get-results.js
@@ -1,40 +1,31 @@
 /* global: location, sessionStorage */
 
-(function resultsPage () {
-  var path = location.pathname.slice(1)
-    if (path === "results") {
-      var insertAnswers = document.getElementById('insert-answers')
-      var surveyType = sessionStorage.getItem('select-survey')
+(function resultsPage() {
 
-        if (surveyType === 'pre-session') {
-          var preSessionQuestions = ['consent', 'today', 'last-week', 'family', 'friends', 'school', 'play', 'next-week']
-            for (var i = 0; i < preSessionQuestions.length; i++){
-              var currentQuestion = preSessionQuestions[i]
-              var logAnswer = sessionStorage.getItem(preSessionQuestions[i])
-                if (logAnswer) {
-                  currentQuestion = currentQuestion.replace('-', ' ')
-                  var addResponse = document.createTextNode(currentQuestion + ': ' + logAnswer)
-                }
-                else {
-                  var addResponse = document.createTextNode(currentQuestion + ': no answer')
-                }
-                var newPara = document.createElement('p')
-                newPara.appendChild(addResponse)
-                var div = document.getElementById('insert-answers')
-                insertAnswers.appendChild(newPara)
-            }
+    var path = location.pathname.slice(1)
+    if (path === 'results') {
+
+      var surveyType = sessionStorage.getItem('select-survey')
+      document.getElementById('survey-type').textContent = surveyType
+
+      if (surveyType === 'pre-session') {
+        var surveyQuestions = ['consent', 'today', 'last-week', 'family', 'friends', 'school', 'play', 'next-week']
+      } else if (surveyType === 'post-session') {
+        var surveyQuestions = ['consent', 'understand', 'help', 'like', 'come-again']
+      }
+
+      for (var i = 0; i < surveyQuestions.length; i++) {
+        var currentQuestion = surveyQuestions[i].replace('-', ' ')
+        var logAnswer = sessionStorage.getItem(surveyQuestions[i])
+        if (logAnswer) {
+          var addResponse = document.createTextNode(currentQuestion + ': ' + logAnswer)
+        } else {
+          var addResponse = document.createTextNode(currentQuestion + ': no answer')
         }
-        else if (surveyType === 'post-session') {
-          var postSessionResponses = ['consent', 'understand', 'help', 'like', 'come-again']
-          var postSessionAnswers = []
-            for (var i = 0; i < postSessionResponses.length; i++){
-              postSessionAnswers.push(sessionStorage.getItem(postSessionResponses[i]))
-            }
-            insertAnswers.textContent = postSessionAnswers
-        }
-        else {
-          insertAnswers.textContent = 'no results available'
-        }
+        var newPara = document.createElement('p')
+        newPara.appendChild(addResponse)
+        var insertAnswers = document.getElementById('insert-answers')
+        insertAnswers.appendChild(newPara)
     }
-    else return
-  })();
+  }
+})();
