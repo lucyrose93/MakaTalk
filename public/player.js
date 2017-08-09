98ed4d1168db385036e802f2ffae4bb40071180b
smarthutza <smarthutza@yahoo.com>
2017-08-09 12:51:51 +0100

    I inserted a script tag containing the playlistID in all the files from the pages folder. All videos are now displayed on the right page in the right order. It is a bit repetitive, but I will refactor at some point.

--- public/player.js
+++ public/player.js
@@ -1,48 +1,48 @@
-  // 2. This code loads the IFrame Player API code asynchronously.
-  var tag = document.createElement('script');
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script')
 
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = 'https://www.youtube.com/iframe_api'
+var firstScriptTag = document.getElementsByTagName('script')[0]
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-  // 3. This function creates an <iframe> (and YouTube player)
-  //    after the API code downloads.
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
 
-  var player;
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      playerVars: {
-        listType: 'playlist',
-        list: playlist_id,
-        controls: 0,
-        showinfo: 0,
-        rel: 0
-      },
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-  }
-
-  // 4. The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-    event.target.playVideo();
-  }
-
-  function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING) {
-      var currentIndex = player.getPlaylistIndex();
-      console.log('My index is ' + currentIndex);
-      switch(currentIndex) {
-    case 1:
-        document.getElementById("yes-consent").style.color = "#ff0000";
-        break;
-    case 2:
-        document.getElementById("no-consent").style.color = "blue";
-        break;
-      }
+var player
+function onYouTubeIframeAPIReady () {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    playerVars: {
+      listType: 'playlist',
+      list: playlist_id,
+      controls: 0,
+      showinfo: 0,
+      rel: 0
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
     }
+  })
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady (event) {
+  event.target.playVideo()
+}
+
+function onPlayerStateChange (event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+    var currentIndex = player.getPlaylistIndex()
+    console.log('My index is ' + currentIndex)
+    //   switch(currentIndex) {
+    // case 1:
+    //     document.getElementById("yes-consent").style.color = "#ff0000";
+    //     break;
+    // case 2:
+    //     document.getElementById("no-consent").style.color = "blue";
+    //     break;
+    //   }
   }
+}
