fb98b68e84689b49386ff3336dd69b517448ce59
lucyrose93 <lucyrose@protonmail.com>
2017-08-10 17:46:06 +0100

    put send results fn in IIFE
    #100

--- public/send-results.js
+++ public/send-results.js
@@ -1,36 +1,26 @@
-//if page is welcome page
+/* global: location, sessionStorage */
 
+(function() {
+  var path = location.pathname.slice(1)
 
+  if (path === "login") {
+    var submitSurveyButton = document.getElementById('submit-survey-button')
+    var clinicianEmailInput = document.getElementById('clinician-email-input')
+    submitSurveyButton.addEventListener('click', function() {
+      var storeClinicianEmail = sessionStorage.setItem('clinician-email', clinicianEmailInput.value)
+    })
+  }
 
-var path = location.pathname.slice(1)
+  if (path === "results") {
+    var sendResultsButton = document.getElementById('send-results-button')
+    var getSessionStorage = window.sessionStorage
+    var getClinicianEmail = getSessionStorage['clinician-email']
+    var emailSubject = getSessionStorage['select-survey'] + ' results ' + Date()
+    var emailBody = JSON.stringify(getSessionStorage)
+    emailBody = emailBody.replace(/[{}"]/g, ' ').slice(emailBody.indexOf(",") + 2, emailBody.lastIndexOf(","))
 
-if (path === "login") {
-  var submitSurveyButton = document.getElementById('submit-survey-button')
-  var clinicianEmailInput = document.getElementById('clinician-email-input')
-  submitSurveyButton.addEventListener('click', function () {
-    var storeClinicianEmail = sessionStorage.setItem('clinician-email', clinicianEmailInput.value)
-  })
-}
+    var mailToClinicianURL = 'mailto:' + getClinicianEmail + '?subject=' + emailSubject + '&body=' + emailBody
 
-if (path === "results"){
-
-  var sendResultsButton = document.getElementById('send-results-button')
-  var getSessionStorage = window.sessionStorage
-  var getClinicianEmail = getSessionStorage['clinician-email']
-  var emailSubject = getSessionStorage['select-survey'] + ' results ' + Date()
-  var emailBody = JSON.stringify(getSessionStorage)
-  emailBody = emailBody.replace(/[{}"]/g, ' ')
-
-  var mailToClinicianURL = 'mailto:' + getClinicianEmail + '?subject=' + emailSubject + '&body=' + emailBody
-
-  sendResultsButton.href = mailToClinicianURL
-
-}
-
-
-
-
-// subject is equal to type of survey & timestamp, body is equal to results
-
-
-// sendResults.addEventListener("click", function(){console.log('hey')})
+    sendResultsButton.href = mailToClinicianURL
+  }
+})()
