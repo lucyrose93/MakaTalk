28ff24abdc5a13d9d30043986d140adc82992f66
lucyrose93 <lucy.rose.sabin@gmail.com>
2017-08-15 20:00:18 +0100

    add array of keys to send results fn
    relates #102

--- public/send-results.js
+++ public/send-results.js
@@ -24,16 +24,18 @@
     if (emailSubject === 'post-session') {
       var questions = ['consent', 'understand', 'help', 'like', 'come-again']
     }
-      for (var i=0; i<questions.length; i++) {
-        var x = storage.getItem(questions[i])
-        if (x) answers.push(x)
-        else {answers.push('no answer')}
+    for (var i = 0; i < questions.length; i++) {
+      var x = storage.getItem(questions[i])
+      if (x) answers.push(questions[i] + ': ' + x)
+      else {
+        answers.push(questions[i] + ': ' + 'no answer')
       }
+    }
+    emailBody = answers.toString().replace(/,/g, ', ')
 
-    // var mailToClinicianURL = 'mailto:' + clinicianEmail + '?subject=' + emailSubject + currentTime + '&body=' + 'hi'
+    var mailToClinicianURL = 'mailto:' + clinicianEmail + '?subject=' + emailSubject + currentTime + '&body=' + emailBody
 
     var sendResultsButton = document.getElementById('send-results-button')
-
-    sendResultsButton.href = "mailto:lucy.rose.sabin@gmail.com"
+    sendResultsButton.href = mailToClinicianURL
   }
 })()
